2bfcd065a7e2d4bf0ba0540aa6a6fdf070395c93
Scott Tianhan Jiang <64949105+ScottJiangTH@users.noreply.github.com>
2020-12-15 15:40:53 -0700

    Completed Section1 element view

--- webapp/fe/src/Section1.js
+++ webapp/fe/src/Section1.js
@@ -7,19 +7,15 @@ function Section1() {
       <section class="hero is-primary">
         <div class="hero-body">
           <div class="container">
-            <h1 class="title">
-              Course Outline Builder
-            </h1>
-            <h2 class="subtitle">
-              1. Calendar Information
-            </h2>
+            <h1 class="title">Course Outline Builder</h1>
+            <h2 class="subtitle">1. Calendar Information</h2>
           </div>
         </div>
       </section>
       <section>
         <div class="field">
-          <label class="label">Course Number</label>
           <div class="control">
+            <label class="label">Course Number</label>
             <input class="input" type="text" placeholder="e.g. ENSF 409"></input>
           </div>
           <div class="control">
@@ -30,6 +26,44 @@ function Section1() {
             <label class="label">Course Description</label>
             <textarea class="textarea is-success" placeholder="A brief Description of the Course"></textarea>
           </div>
+          <div class="control has-icons-left">
+            <label class="label">Course Hours</label>
+            <div class="select is-small">
+              <select>
+                <option selected>3 units; H (3-2)</option>
+                <option>4 units; H (4-4)</option>
+                <option>2 units; H (2-2)</option>
+                <option>1 units; H (1-1)</option>
+              </select>
+            </div>
+            <span class="icon is-small is-left">
+              <i class="fas fa-globe"></i>
+            </span>
+          </div>
+          <div class="control has-icons-left">
+            <label class="label">Academic Credit</label>
+            <div class="select is-small">
+              <select>
+                <option selected>3</option>
+                <option>4</option>
+                <option>2</option>
+                <option>1</option>
+              </select>
+            </div>
+            <span class="icon is-small is-left">
+              <i class="fas fa-globe"></i>
+            </span>
+          </div>
+          <div class="control">
+            <label class="label">Calendar Reference</label>
+            <input class="input" type="text" placeholder="e.g. http://www.ucalgary.ca/pubs/calendar/current/software-engineering-for-engineers.html#38252"></input>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div class="buttons">
+          <button class="button is-link">Confirm</button>
+          <button class="button is-link">Clear</button>
         </div>
       </section>
     </html>
