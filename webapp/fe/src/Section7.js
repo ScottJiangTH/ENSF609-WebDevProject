b712382e3ef65817a30134054210ea160e9bdc73
Scott Tianhan Jiang <64949105+ScottJiangTH@users.noreply.github.com>
2020-12-16 13:48:29 -0700

    Created Section7

---
+++ webapp/fe/src/Section7.js
@@ -0,0 +1,18 @@
+import 'bulma/css/bulma.css';
+function Section7() {
+    return (
+        <html>
+            <section class="hero is-dark has-bg-img">
+                <div class="hero-body">
+                    <img is-bg-img src="https://upload.wikimedia.org/wikipedia/en/3/3f/Schulich.png"></img>
+                    <div class="container has-text-right">
+                        <h1 class="title">Course Outline Builder</h1>
+                        <h2 class="subtitle">7. Final Grade Determination</h2>
+                    </div>
+                </div>
+            </section>
+        </html >
+    );
+}
+
+export default Section7;
