676e7024cdecf9caefc1a3a5fd9da4c7980de766
Scott Tianhan Jiang <64949105+ScottJiangTH@users.noreply.github.com>
2021-01-11 02:04:15 -0700

    Added part of DB tables in models.py

--- webapp/be/WebappApi/courses/models.py
+++ webapp/be/WebappApi/courses/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    courseNumber = models.CharField(max_length=70, null=False, primary_key=True)
+    courseName = models.CharField(max_length=500, null=False)
+    courseDescription = models.CharField(max_length=2000, null=True)
+    academicCredit = models.PositiveIntegerField(null=False)
+    lectureHours = models.PositiveIntegerField(null=False)
+    labHours = models.PositiveIntegerField(null=False)
+    refUrl = models.CharField(max_length=2000, null=True)
+
+class Outcome(models.Model):
+    id = models.PositiveIntegerField(primary_key=True)
+    description = models.CharField(max_length=2000, null=False)
+    courseNumber = models.ForeignKey(Course, on_delete=models.CASCADE, primary_key=True)
+
+class GraduateAttribute(models.Model):
+    id = models.CharField(max_length=10, null=False, primary_key=True)
+    courseNumber = models.ForeignKey(Course, on_delete=models.CASCADE, primary_key=True)
+    oid = models.ForeignKey(Outcome, on_delete=models.CASCADE, primary_key=True)
+    description = models.CharField(max_length=2000, null=True)
+    instructionLevel = models.CharField(max_length=50, null=True)
+
+# to be continued
